55599f3510fdd7217f02c82bac49e05a7413c36b
Zinenko Daniil Vitalevich <daniil.zinenko@lamoda.ru>
2020-03-08 13:03:12 +0300

    aws lambda test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bobrnor/getchanski
 
 go 1.14
+
+require github.com/aws/aws-lambda-go v1.15.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type EnqueueURLRequest struct {
+	URL string
+}
+
+type EnqueueURLResponse struct {
+	Status string
+}
+
+func HandleRequest(ctx context.Context, event EnqueueURLRequest) (EnqueueURLResponse, error) {
+	return EnqueueURLResponse{
+		Status: fmt.Sprintf("Enqueued: %s", event.URL),
+	}, nil
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	lambda.Start(HandleRequest)
 }
